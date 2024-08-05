923bf5dec8608788c18f269efe76d70ff90dc78c
Jaime <jaimecobo@outlook.com>
2024-08-05 09:26:42 -0500

    VisitedCityController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/VisitedCityController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/VisitedCityController.java
@@ -1,2 +1,16 @@
-package co.jaimecobo.javaspringmaven0724.controller;public class VisitedCityController {
+package co.jaimecobo.javaspringmaven0724.controller;
+
+import co.jaimecobo.javaspringmaven0724.database.dao.VisitedCityDAO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/visitedcity")
+public class VisitedCityController {
+
+    @Autowired
+    private VisitedCityDAO visitedCityDAO;
 }
