ba098159ce16418a42641f2a7f82ce002a622386
Jaime <jaimecobo@outlook.com>
2024-08-04 22:18:39 -0500

    CityDAO.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/CityDAO.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/CityDAO.java
@@ -1,2 +1,14 @@
-package co.jaimecobo.javaspringmaven0724.database.dao;public interface CityDAO {
+package co.jaimecobo.javaspringmaven0724.database.dao;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CityDAO extends JpaRepository<City, Long> {
+    City findById(Integer id);
+
+    List<City> findByNameIgnoreCase(String name);
+    City findByNameAndStateProvinceDepartmentTerritoryAndCountryIgnoreCase(String name, String stateProvinceDepartmentTerritory, String country);
+
 }
