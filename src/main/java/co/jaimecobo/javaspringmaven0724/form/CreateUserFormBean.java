71a5f474d048280b29c5fba754bf84f99da53013
Jaime <jaimecobo@outlook.com>
2024-08-05 09:28:59 -0500

    CreateUserFormBean.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/form/CreateUserFormBean.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/form/CreateUserFormBean.java
@@ -2,7 +2,6 @@ package co.jaimecobo.javaspringmaven0724.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,47 +13,65 @@ import java.util.Date;
 @Getter
 @Setter
 @ToString
-public class CreateCityFormBean {
+public class CreateUserFormBean {
 
-    private Integer cityId;
+    private Integer userId;
+
+    @Length(max = 50, message = "Username must be less than 50 characters")
+    @NotEmpty(message="Username name is required")
+    private String username;
+
+    @Email(message = "Please enter a valid email")
+    @NotEmpty(message="Email is required")
+    private String email;
+
+    @Length(max = 50, message = "Password must be less than 50 characters")
+    @NotEmpty(message = "Password is required")
+    private String password;
+
+    @Length(max = 50, message = "Brand/Nickname/Company must be less than 50 characters")
+    private String brandNicknameCompany;
+
+    @Length(max = 50, message = "First name must be less than 50 characters")
+    @NotEmpty(message="First name is required")
+    private String firstName;
+
+    @Length(max = 50, message = "Last name must be less than 50 characters")
+    @NotEmpty(message="Last name name is required")
+    private String lastName;
+
+    @Length(max = 20, message = "Gender must be less than 20 characters")
+    @NotEmpty(message="Gender is required")
+    private String gender;
+
+    @Length(max = 11, message = "Phone must be less than 12 characters")
+    @NotEmpty(message = "Phone number is required")
+    private String phone;
+
+    @Length(max = 50, message = "Address must be less than 50 characters")
+    @NotEmpty(message="Address is required")
+    private String address;
 
     @Length(max = 50, message = "City name must be less than 50 characters")
     @NotEmpty(message="City name is required")
-    private String name;
+    private String city;
 
-    @Length(max = 50, message = "City name must be less than 50 characters")
-    @NotEmpty(message="Please enter the state or province or department or territory corresponding to this city")
-    private String stateProvinceDepartmentTerritory;
+    @Length(max = 50, message = "State/Province/Department/Territory name must be less than 50 characters")
+    @NotEmpty(message="Please enter the state or province or department or territory corresponding to your city")
+    private String state;
+
+    @Length(max = 15, message = "Zip code must be less than 15 characters")
+    @NotEmpty(message = "Zip code is required")
+    private String zipCode;
 
     @Length(max = 50, message = "Country must be less than 50 characters")
     @NotEmpty(message = "Country is required")
     private String country;
 
-    @Length(max = 100, message = "Slogan must be less than 100 characters")
-    @NotEmpty(message = "Slogan is required")
-    // TO DO = Add validation for unique slogan
-    private String slogan;
-
-    @Length(max = 512, message = "Description must be less than 512 characters")
-    @NotEmpty(message = "Description is required")
-    // TO DO = Add validation for unique description
-    private String description;
-
-//    @NotEmpty(message = "City iconic image is required")
-    // TO DO = Add validation for unique cityImageUrl
-    private String cityImageUrl;
-
-    @NotEmpty(message = "City most representative website URL is required")
-    // TO DO = Add validation for unique cityWebUrl
-    private String cityWebUrl;
-
-    private Integer LastEditorUser;
+    private String userImageUrl;
 
     private Date createdAt;
 
-    private Date editedDate;
-
-//    @NotEmpty(message = "City iconic image is required")
     private MultipartFile file;
 
 }
