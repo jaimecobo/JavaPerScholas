f2dac500ec84e2583aef8209dc308efba08a27eb
Jaime <jaimecobo@outlook.com>
2024-08-04 20:03:18 -0500

    .\Relationship.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/entity/Relationship.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/entity/Relationship.java
@@ -1,2 +1,38 @@
-package co.jaimecobo.javaspringmaven0724.database.entity;public class Relationship {
+package co.jaimecobo.javaspringmaven0724.database.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "relationships")
+public class Relationship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User followerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_id", nullable = false)
+    private User followedId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "relationship_type", nullable = false)
+    private RelationshipType relationshipType;
+
+    @Column(name = "created_at", updatable = false,  nullable = false, columnDefinition = "timestamp default current_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 }
