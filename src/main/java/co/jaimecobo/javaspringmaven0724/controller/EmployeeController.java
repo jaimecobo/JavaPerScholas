842e521e7cf1490930514cb19a1513c126e324ef
Jaime <jaimecobo@outlook.com>
2024-08-05 09:22:52 -0500

    EmployeeController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/EmployeeController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package co.jaimecobo.javaspringmaven0724.controller;
 import co.jaimecobo.javaspringmaven0724.database.dao.EmployeeDAO;
 import co.jaimecobo.javaspringmaven0724.database.entity.Employee;
 import co.jaimecobo.javaspringmaven0724.form.CreateEmployeeFormBean;
+import co.jaimecobo.javaspringmaven0724.security.AuthenticatedEmployeeUtilities;
+import co.jaimecobo.javaspringmaven0724.security.AuthenticatedUserUtilities;
 import co.jaimecobo.javaspringmaven0724.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +14,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
-public class LoginController {
+@RequestMapping("/employee")
+public class EmployeeController {
 
     @Autowired
     private EmployeeDAO employeeDAO;
@@ -27,25 +28,49 @@ public class LoginController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private AuthenticatedEmployeeUtilities authenticatedEmployeeUtilities;
+
+    @Autowired
+    private AuthenticatedUserUtilities authenticatedUserUtilities;
+
+
+    @GetMapping("/employees")
+    public ModelAndView searchEmployee(@RequestParam(required = false) String email) {
+        ModelAndView response = new ModelAndView("employee/employees"); //Here employees is the exact same name of employees.jsp
+        response.addObject("email", email); //Here employee is the variable that we are passing or returning to the web page
+        Employee employee = employeeDAO.findByEmailIgnoreCase(email);
+        response.addObject("employee", employee);
+        return response;
+    }
+
+
     @GetMapping({"/employee/{id}"})
     public ModelAndView employeeById(@PathVariable Integer id) {
-        ModelAndView response = new ModelAndView("auth/employee");
+        ModelAndView response = new ModelAndView("employee/employee");
         log.debug("The user has selected a employee with id = " + id);
         Employee employee = employeeDAO.findById(id);
         response.addObject("employeeKey", employee);
         return response;
     }
 
+
     @GetMapping("/create-employee")
     public ModelAndView createEmployee() {
-        ModelAndView response = new ModelAndView("auth/create-employee");
-
-        return response;
+        return new ModelAndView("employee/create-employee");
     }
 
+
     @PostMapping("/submit-employee")
-    public ModelAndView createEmployeeSubmit(@Valid CreateEmployeeFormBean form, BindingResult bindingResult){
-        ModelAndView response = new ModelAndView("auth/create-employee");
+    public ModelAndView createEmployeeSubmit(HttpSession session, @Valid CreateEmployeeFormBean form, BindingResult bindingResult){
+        ModelAndView response = new ModelAndView("employee/create-employee");
+
+        if ( form.getEmployeeId() == null ) {
+            Employee e = employeeDAO.findByEmailIgnoreCase(form.getEmail());
+            if ( e != null ) {
+                bindingResult.rejectValue("email", "email", "Try with a different email. Checking from ctrl");
+            }
+        }
 
         if (bindingResult.hasErrors()) {
             for (ObjectError error : bindingResult.getAllErrors()) {
@@ -53,9 +78,17 @@ public class LoginController {
             }
             response.addObject("bindingResult", bindingResult);
             response.addObject("form", form);
+            response.setViewName("employee/create-employee");
         }
         else{
-            response.setViewName("redirect:/account/" + employeeService.createEmployee(form).getId());
+            // This line was creating the new employee and redirecting to their new create employee details page
+//            response.setViewName("redirect:/employee/employee/" + employeeService.createEmployee(form).getId());
+
+            // These to lines instead of the line above, is creating the new employee and logging in the employee automatically
+            employeeService.createEmployee(form);
+//            authenticatedUserUtilities.manualAuthentication(session, form.getEmail(), form.getPassword());
+            authenticatedEmployeeUtilities.manualAuthentication(session, form.getEmail(), form.getPassword());
+
         }
         return response;
 
@@ -63,7 +96,7 @@ public class LoginController {
 
     @GetMapping({"/edit-employee"})
     public ModelAndView editEmployee(@RequestParam(required = false) Integer employeeId) {
-        ModelAndView response = new ModelAndView("auth/create-employee");
+        ModelAndView response = new ModelAndView("employee/create-employee");
         if(employeeId != null) {
             Employee employee = employeeDAO.findById(employeeId);
             if(employee != null) {
@@ -73,6 +106,9 @@ public class LoginController {
                 form.setFullName(employee.getFullName());
                 form.setPassword(employee.getPassword());
                 response.addObject("form", form);
+            }
+            else {
+                response.addObject("errorMessage", "Employee was not found!");
             }
         }
         return response;
