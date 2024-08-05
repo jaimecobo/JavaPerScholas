bb0e2aea3d2aed855f6e61b5d8d3f5077bfe6bd3
Jaime <jaimecobo@outlook.com>
2024-08-05 09:21:19 -0500

    CityController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/CityController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/CityController.java
@@ -1,2 +1,129 @@
-package co.jaimecobo.javaspringmaven0724.controller;public class CityController {
+package co.jaimecobo.javaspringmaven0724.controller;
+
+import co.jaimecobo.javaspringmaven0724.database.dao.CityDAO;
+import co.jaimecobo.javaspringmaven0724.database.dao.EmployeeDAO;
+import co.jaimecobo.javaspringmaven0724.database.dao.UserDAO;
+import co.jaimecobo.javaspringmaven0724.database.entity.City;
+import co.jaimecobo.javaspringmaven0724.database.entity.Employee;
+import co.jaimecobo.javaspringmaven0724.database.entity.User;
+import co.jaimecobo.javaspringmaven0724.form.CreateCityFormBean;
+import co.jaimecobo.javaspringmaven0724.service.CityService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/city")
+public class CityController {
+
+    @Autowired
+    private CityDAO cityDAO;
+
+    @Autowired
+    private CityService cityService;
+
+//    @Autowired
+//    private UserDAO userDAO;
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+
+    @GetMapping("/cities")
+    public ModelAndView searchCity(@RequestParam(required = false) String name) {
+        ModelAndView response = new ModelAndView("city/cities");
+        response.addObject("city", name);
+
+        List<City> cityList = cityDAO.findByNameIgnoreCase(name);
+        response.addObject("cities", cityList);
+        return response;
+    }
+
+
+    @GetMapping({"/city/{id}"})
+    public ModelAndView cityById(@PathVariable Integer id) {
+        ModelAndView response = new ModelAndView("city/city");
+        log.debug("The user has selected a city with id = " + id);
+        City city = cityDAO.findById(id);
+        response.addObject("cityKey", city);
+//        User user = userDAO.findById(city.getLastEditorUser());
+//        response.addObject("userKey", user);
+        Employee user = employeeDAO.findById(city.getLastEditorUser());
+        response.addObject("userKey", user);
+
+        return response;
+    }
+
+    @GetMapping("/create-city")
+    public ModelAndView createCity() {
+        return new ModelAndView("city/create-city");
+    }
+
+    @PostMapping("/submit-city")
+    public ModelAndView createCitySubmit(@Valid CreateCityFormBean form, BindingResult bindingResult){
+        ModelAndView response = new ModelAndView("city/create-city");
+        if ( form.getCityId() == null ) {
+            City c = cityDAO.findByNameAndStateProvinceDepartmentTerritoryAndCountryIgnoreCase(form.getName(), form.getStateProvinceDepartmentTerritory(), form.getCountry());
+            if ( c != null ) {
+                bindingResult.rejectValue("cityName", "cityName", "This city already exists! Checking from ctrl");
+            }
+        }
+//        log.info("##### The file received is  = " + form.getFile().getOriginalFilename() + " #####");
+        if(!cityService.validateImage(form.getFile())){
+            bindingResult.rejectValue("cityImageUrl", "cityImageUrl", "Make sure your image is a .jpg, .jpeg or .png file and its size isn't larger than 6MB! Checking from ctrl.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                log.debug("Validation error : " + ((FieldError) error).getField() + " = " + error.getDefaultMessage());
+            }
+            response.addObject("bindingResult", bindingResult);
+            response.addObject("form", form);
+            response.setViewName("city/create-city");
+        }
+        else{
+            response.setViewName("redirect:/city/city/" + cityService.createCity(form).getId());
+        }
+        return response;
+
+    }
+
+
+    @GetMapping({"/edit-city"})
+    public ModelAndView editCity(@RequestParam(required = false) Integer cityId) {
+        ModelAndView response = new ModelAndView("city/create-city");
+        if(cityId != null) {
+            City city = cityDAO.findById(cityId);
+            if(city != null) {
+                CreateCityFormBean form = new CreateCityFormBean();
+                form.setCityId(city.getId());
+                form.setName(city.getName());
+                form.setStateProvinceDepartmentTerritory(city.getStateProvinceDepartmentTerritory());
+                form.setCountry(city.getCountry());
+                form.setSlogan(city.getSlogan());
+                form.setDescription(city.getDescription());
+                form.setCityImageUrl(form.getCityImageUrl());
+                form.setCityWebUrl(city.getCityWebUrl());
+                form.setLastEditorUser(city.getLastEditorUser());
+                form.setEditedDate(city.getEditedDate());
+
+                response.addObject("form", form);
+            }
+            else {
+                response.addObject("errorMessage", "City was not found!");
+            }
+        }
+        return response;
+
+    }
+
 }
