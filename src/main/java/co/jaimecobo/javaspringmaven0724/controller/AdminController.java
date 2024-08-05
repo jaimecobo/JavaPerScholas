761c3ea6dd1deb55b1237f77bd02d2627ee25572
Jaime <jaimecobo@outlook.com>
2024-08-05 09:20:58 -0500

    AdminController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/AdminController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/AdminController.java
@@ -1,2 +1,46 @@
-package co.jaimecobo.javaspringmaven0724.controller;public class AdminController {
+package co.jaimecobo.javaspringmaven0724.controller;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.Employee;
+import co.jaimecobo.javaspringmaven0724.security.AuthenticatedEmployeeUtilities;
+import co.jaimecobo.javaspringmaven0724.security.AuthenticatedUserUtilities;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasAnyAuthority('ADMIN')")           // This annotation can be used here at the class level or directly on a method
+public class AdminController {
+//
+//    @Autowired
+//    private final AuthenticatedUserUtilities authenticatedUserUtilities;
+//
+//    public AdminController(AuthenticatedUserUtilities authenticatedUserUtilities) {
+//        this.authenticatedUserUtilities = authenticatedUserUtilities;
+//    }
+
+    @Autowired
+    private AuthenticatedEmployeeUtilities authenticatedEmployeeUtilities;
+
+    public AdminController(AuthenticatedEmployeeUtilities authenticatedEmployeeUtilities) {
+        this.authenticatedEmployeeUtilities = authenticatedEmployeeUtilities;
+    }
+
+
+    //    @PreAuthorize("hasAnyAuthority('ADMIN')")     // This annotation can be used here on the method level or at the class
+    @GetMapping("dashboard")
+    public ModelAndView dashboard() {
+        ModelAndView response = new ModelAndView("admin/dashboard");
+//        Employee employee  = authenticatedUserUtilities.getCurrentUser();
+        Employee employee  = authenticatedEmployeeUtilities.getCurrentEmployee();
+        log.info(employee.toString());
+        response.addObject("employeeKey", employee);
+        return response;
+    }
+
 }
