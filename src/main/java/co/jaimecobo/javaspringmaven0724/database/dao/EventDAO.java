305197740dcf21e481f854bfb8cac5880b7e3303
Jaime <jaimecobo@outlook.com>
2024-08-04 22:20:14 -0500

    EventDAO.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/EventDAO.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/EventDAO.java
@@ -1,2 +1,12 @@
-package co.jaimecobo.javaspringmaven0724.database.dao;public interface EventDAO {
+package co.jaimecobo.javaspringmaven0724.database.dao;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventDAO extends JpaRepository<Event, Long> {
+    Event findById(Integer id);
+
+    List<Event> findByOrganizerId(Integer id);
 }
