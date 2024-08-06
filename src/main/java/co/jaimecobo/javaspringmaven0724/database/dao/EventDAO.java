502448f44976aaa9a907c2dc373dbaec64caf137
Jaime <jaimecobo@outlook.com>
2024-08-05 21:09:55 -0500

    EventDAO.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/EventDAO.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/EventDAO.java
@@ -3,10 +3,16 @@ package co.jaimecobo.javaspringmaven0724.database.dao;
 import co.jaimecobo.javaspringmaven0724.database.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface EventDAO extends JpaRepository<Event, Long> {
     Event findById(Integer id);
-
     List<Event> findByOrganizerId(Integer id);
+    List<Event> findByNameIgnoreCase(String eventName);
+
+//    Event findByNameAndCityIdAndStartingDateIgnoreCase(String name, Integer cityId, Date startingDate);
+//    Event findByNameAndCityIdAndStartingDateIgnoreCase(String name, Integer cityId, String startingDate);
+
+//    Event findByNameAndCityIdIgnoreCase(String name, Integer cityId);
 }
---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/form/CreateEventFormBean.java
@@ -0,0 +1,2 @@
+package co.jaimecobo.javaspringmaven0724.form;public class CreateEventFormBean {
+}
