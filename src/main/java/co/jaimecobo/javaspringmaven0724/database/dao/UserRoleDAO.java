ab92587b60e923c36c38f2404d55e900678f07eb
Jaime <jaimecobo@outlook.com>
2024-08-07 00:11:59 -0500

    080724 commit UserRoleDAO.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/UserRoleDAO.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/UserRoleDAO.java
@@ -1,11 +1,11 @@
 package co.jaimecobo.javaspringmaven0724.database.dao;
 
-import co.jaimecobo.javaspringmaven0724.database.entity.Employee;
-import co.jaimecobo.javaspringmaven0724.database.entity.EmployeeRole;
+import co.jaimecobo.javaspringmaven0724.database.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmployeeRoleDAO extends JpaRepository<EmployeeRole, Long> {
-    List<EmployeeRole> findByEmployeeId(Integer id);
+public interface UserRoleDAO extends JpaRepository<UserRole, Long> {
+    List<UserRole> findByUserId(Integer id);
+
 }
