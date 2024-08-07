59c146ddd6eb77f19d1116e34aeff9abda976da7
Jaime <jaimecobo@outlook.com>
2024-08-07 00:12:54 -0500

    080724 commit VisitedCityDAO.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/VisitedCityDAO.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/VisitedCityDAO.java
@@ -8,11 +8,13 @@ import java.util.List;
 import java.util.Map;
 
 public interface VisitedCityDAO extends JpaRepository<VisitedCity, Long> {
+    VisitedCity findByUserIdAndCityId(Integer id, Integer id1);
+
     VisitedCity findByCityId(VisitedCity visitedCityId);
 
-    @Query(value="SELECT c.name AS city, c.country, c.slogan, c.description, c.city_image_url AS image, c.city_web_url AS web " +
+    @Query(value="SELECT c.id, c.name AS city, c.state_province_department_territory as state, c.country, c.slogan, c.description, c.city_image_url AS image, c.city_web_url AS web " +
             "FROM visited_cities vc, cities c " +
-            "WHERE vc.city_id = c.id AND vc.user_id = :id " +
+            "WHERE vc.city_id = c.id AND vc.user_id = :id AND city_visited = 1 " +
             "ORDER BY city;",
             nativeQuery = true)
     List<Map<String, Object>> findByUserId(Integer id);
