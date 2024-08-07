930d49a1ab65a9f5235778aab3c2d557192b8520
Jaime <jaimecobo@outlook.com>
2024-08-07 00:18:48 -0500

    080724 commit EventController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/EventController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/EventController.java
@@ -1,10 +1,24 @@
 package co.jaimecobo.javaspringmaven0724.controller;
 
+import co.jaimecobo.javaspringmaven0724.database.dao.CityDAO;
 import co.jaimecobo.javaspringmaven0724.database.dao.EventDAO;
+import co.jaimecobo.javaspringmaven0724.database.dao.UserDAO;
+import co.jaimecobo.javaspringmaven0724.database.entity.City;
+import co.jaimecobo.javaspringmaven0724.database.entity.Event;
+import co.jaimecobo.javaspringmaven0724.database.entity.User;
+import co.jaimecobo.javaspringmaven0724.form.CreateEventFormBean;
+import co.jaimecobo.javaspringmaven0724.service.EventService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -13,4 +27,122 @@ public class EventController {
 
     @Autowired
     private EventDAO eventDAO;
+
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private CityDAO cityDAO;
+
+    @GetMapping("/events")
+    public ModelAndView searchEvent(@RequestParam(required = false) String name) {
+        ModelAndView response = new ModelAndView("event/events");
+        response.addObject("eventKey", name);
+
+        List<Event> eventsList = eventDAO.findByNameIgnoreCase(name);
+        response.addObject("eventsKey", eventsList);
+        return response;
+    }
+
+
+    @GetMapping({"/event/{id}"})
+    public ModelAndView eventById(@PathVariable Integer id) {
+        ModelAndView response = new ModelAndView("event/event");
+        log.debug("The user has selected an event with id = " + id);
+        Event event = eventDAO.findById(id);
+        response.addObject("eventKey", event);
+        User user = userDAO.findById(event.getOrganizerId());
+        response.addObject("userKey", user);
+        return response;
+
+    }
+
+    @GetMapping("/create-event")
+    public ModelAndView createEvent() {
+        // Because the page needs the list of cities for the dropdown, we need to add the list of cities to the model
+        List<City> ListOfCities = cityDAO.findAllOrderedByCity();
+        ModelAndView response = new ModelAndView("event/create-event");
+        response.addObject("citiesKey", ListOfCities);
+        return response;
+    }
+
+
+    @PostMapping("/submit-event")
+    public ModelAndView createEventSubmit(@Valid CreateEventFormBean form, BindingResult bindingResult){
+        ModelAndView response = new ModelAndView("event/create-event");
+
+        if ( form.getEventId() == null ) {
+            Event event = eventDAO.findByNameAndCityIdAndStartingDateIgnoreCase(form.getName(), form.getCityId(), form.getStartingDate());
+//            Event event = eventDAO.findByNameAndStartingDateIgnoreCase(form.getName(), form.getStartingDate());
+//            List<Event> events = eventDAO.findByNameIgnoreCase(form.getName());
+//            if ( !events.isEmpty() ) {
+            if ( event != null ) {
+                    bindingResult.rejectValue("name", "name", "This event already exists! Checking from ctrl");
+            }
+        }
+
+        if(!eventService.validateImage(form.getFile())){
+            bindingResult.rejectValue("eventImageUrl", "eventImageUrl", "Make sure your image is a .jpg, .jpeg or .png file and its size isn't larger than 6MB! Checking from ctrl.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                log.debug("Validation error : " + ((FieldError) error).getField() + " = " + error.getDefaultMessage());
+            }
+
+            // Because the page needs the list of cities for the dropdown, we need to add the list of cities to the model
+            List<City> ListOfCities = cityDAO.findAllOrderedByCity();
+            response.addObject("citiesKey", ListOfCities);
+
+            response.addObject("bindingResult", bindingResult);
+            response.addObject("form", form);
+            response.setViewName("event/create-event");
+        }
+        else{
+            // This line creates the new event and redirects to their newly created event details page
+            response.setViewName("redirect:/event/event/" + eventService.createEvent(form).getId());
+
+        }
+        return response;
+
+    }
+
+
+    @GetMapping({"/edit-event"})
+    public ModelAndView editEvent(@RequestParam(required = false) Integer eventId) {
+        ModelAndView response = new ModelAndView("event/create-event");
+        // Because the page needs the list of cities for the dropdown, we need to add the list of cities to the model
+        List<City> ListOfCities = cityDAO.findAllOrderedByCity();
+        response.addObject("citiesKey", ListOfCities);
+
+        if(eventId != null) {
+            Event event = eventDAO.findById(eventId);
+            if(event != null) {
+                CreateEventFormBean form = new CreateEventFormBean();
+                form.setEventId(event.getId());
+                form.setName(event.getName());
+                form.setDescription(event.getDescription());
+                form.setCityId(event.getCityId());
+                form.setOrganizerId(event.getOrganizerId());
+                form.setStartingDate(event.getStartingDate());
+                form.setEndingDate(event.getEndingDate());
+                form.setEventWebUrl(event.getEventWebUrl());
+                form.setEventImageUrl(event.getEventImageUrl());
+                response.addObject("form", form);
+            }
+            else {
+                response.addObject("errorMessage", "Event was not found!");
+            }
+        }
+        return response;
+    }
+
+
+
+
+
+
 }
