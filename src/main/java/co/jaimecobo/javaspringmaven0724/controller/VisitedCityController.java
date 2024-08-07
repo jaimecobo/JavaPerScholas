f0cd986ccef2e3043b02c94fbc8d40dc30404b7f
Jaime <jaimecobo@outlook.com>
2024-08-07 00:20:33 -0500

    080724 commit VisitedCityController.java

--- src/main/java/co/jaimecobo/javaspringmaven0724/controller/VisitedCityController.java
+++ src/main/java/co/jaimecobo/javaspringmaven0724/controller/VisitedCityController.java
@@ -1,10 +1,12 @@
 package co.jaimecobo.javaspringmaven0724.controller;
 
 import co.jaimecobo.javaspringmaven0724.database.dao.VisitedCityDAO;
+import co.jaimecobo.javaspringmaven0724.service.VisitedCityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -13,4 +15,15 @@ public class VisitedCityController {
 
     @Autowired
     private VisitedCityDAO visitedCityDAO;
+    @Autowired
+    private VisitedCityService visitedCityService;
+
+    @GetMapping("/SaveAsVisitedCity")
+    public ModelAndView createUser(@RequestParam Integer status, @RequestParam Integer cityId) {
+        ModelAndView response = new ModelAndView("city/city");
+            response.setViewName("redirect:/city/city/" + visitedCityService.createVisitedCity(status, cityId).getId());
+        return response;
+    }
+
+
 }
